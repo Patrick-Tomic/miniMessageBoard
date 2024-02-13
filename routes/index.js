5e70029a7d3cd55491988d6f1869ccf4b8b99083
Patrick-Tomic <pat12tomic@gmail.com>
2024-02-13 17:01:05 -0500

    connections between form and index, cool stuff

--- routes/index.js
+++ routes/index.js
@@ -29,4 +29,12 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Soprano Quotes', messages:messages });
 });
 
+router.get('/new', function(req, res, next) {
+  res.render('form')
+});
+
+router.post('/new', function(req, res) {
+  messages.push({text:req.body.text, user: req.body.user, date:new Date()})
+  res.redirect('/')
+})
 module.exports = router;
